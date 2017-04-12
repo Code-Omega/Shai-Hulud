b5bf5835bd1e1134a13bb592ee866667da781c28
dynastyreaper <dynastyreaper@hotmail.com>
2017-04-12 10:22:21 -0500

    Some conflict with worm comm.ipynb

---
+++ Shai-Hulud.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# normal configuration
+CND_ARTIFACT_DIR_normal=dist/normal/production
+CND_ARTIFACT_NAME_normal=Shai-Hulud.X.production.hex
+CND_ARTIFACT_PATH_normal=dist/normal/production/Shai-Hulud.X.production.hex
+CND_PACKAGE_DIR_normal=${CND_DISTDIR}/normal/package
+CND_PACKAGE_NAME_normal=shai-hulud.x.tar
+CND_PACKAGE_PATH_normal=${CND_DISTDIR}/normal/package/shai-hulud.x.tar
+# 3_3V configuration
+CND_ARTIFACT_DIR_3_3V=dist/3_3V/production
+CND_ARTIFACT_NAME_3_3V=Shai-Hulud.X.production.hex
+CND_ARTIFACT_PATH_3_3V=dist/3_3V/production/Shai-Hulud.X.production.hex
+CND_PACKAGE_DIR_3_3V=${CND_DISTDIR}/3_3V/package
+CND_PACKAGE_NAME_3_3V=shai-hulud.x.tar
+CND_PACKAGE_PATH_3_3V=${CND_DISTDIR}/3_3V/package/shai-hulud.x.tar
